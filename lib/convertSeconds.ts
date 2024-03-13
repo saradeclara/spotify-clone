4fec678ebfeb2a07ce3a49d65d2bcb2b5e2a328c
Sara De Clara <saradeclara@gmail.com>
2024-03-13 15:18:05 +0000

    fixed description rendering for playlist pages. documentation added to helpers.

--- lib/convertSeconds.ts
+++ lib/convertSeconds.ts
@@ -1,8 +1,16 @@
 /**
- * converts seconds (s) to HH:MM:SS format
- * @param durationInSeconds duration of a song in seconds
- * @returns string containing number of hours, minutes and seconds
+ * The `convertSeconds` function in TypeScript converts a duration in seconds to a formatted time
+ * string.
+ * @param {number | undefined} durationInSeconds - The `durationInSeconds` parameter represents the
+ * total duration in seconds that you want to convert into a time format.
+ * @param {string} [format] - The `format` parameter in the `convertSeconds` function allows you to
+ * specify how you want the output to be formatted. If you provide the value "hhhh mmmm ssss" for the
+ * `format` parameter, the function will return the time in an expanded format with hours, minutes,
+ * @returns The `convertSeconds` function returns either an expanded time string in the format "hhhh
+ * mmmm ssss" or a default time string in the format "hh:mm:ss" based on the input parameters. If the
+ * `durationInSeconds` is undefined, it returns "00:00:00".
  */
+
 const convertSeconds = (
 	durationInSeconds: number | undefined,
 	format?: string
@@ -31,10 +39,24 @@ const convertSeconds = (
 	}
 };
 
+/**
+ * The function `numberOfDigits` takes a number as input and returns the number of digits in that
+ * number.
+ * @param {number} number - The `number` parameter is a number that you want to determine the number of
+ * digits for.
+ * @returns The function `numberOfDigits` returns the number of digits in the input number.
+ */
 const numberOfDigits = (number: number) => {
 	return number.toString().split("").length;
 };
 
+/**
+ * The function `renderExpandedString` takes a duration object with hours, minutes, and seconds
+ * properties and returns a formatted string representing the duration in hours, minutes, and seconds.
+ * @param duration - {
+ * @returns The function `renderExpandedString` returns a string that represents the duration object in
+ * an expanded format, including hours, minutes, and seconds.
+ */
 const renderExpandedString = (duration: {
 	h: number;
 	m: number;
@@ -49,6 +71,17 @@ const renderExpandedString = (duration: {
 	return tempArray.join(" ");
 };
 
+/**
+ * The `renderString` function takes an object with hours, minutes, and seconds properties, formats
+ * them as a time string (HH:MM:SS), and removes leading zeros from hours if present.
+ * @param duration - The `duration` parameter is an object with three properties: `h` for hours (a
+ * number), `m` for minutes (a number), and `s` for seconds (a number).
+ * @returns The function `renderString` takes an object `duration` with properties `h`, `m`, and `s`
+ * representing hours, minutes, and seconds respectively. It converts each value to a string, ensuring
+ * that each value is represented with two digits (e.g., adding a leading zero if necessary). If the
+ * hours value is "00", it is removed from the array. Finally, the function prints out the time string
+ * in the HH:MM:SS format.
+ */
 const renderString = (duration: { h: number; m: number; s: number }) => {
 	let tempArray: string[] = [];
 	Object.values(duration).map((el) => {
--- src/pages/favourites/liked-songs.tsx
+++ src/pages/favourites/liked-songs.tsx
@@ -1,32 +1,52 @@
 import GradientLayoutPages from "@/components/GradientLayoutPages";
 import TopList from "@/components/TopList/TopList";
+import { LoggedInUserContext } from "@/context/LoggedInUserContext";
 import { fetchFavouriteSongs } from "@/react-query/fetch";
 import { favouriteSongsKey } from "@/react-query/queryKeys";
-import { Box } from "@chakra-ui/react";
+import { Avatar, Box, Text } from "@chakra-ui/react";
+import { Song } from "@prisma/client";
+import { useContext } from "react";
 import { useQuery } from "react-query";
+import convertSeconds from "../../../lib/convertSeconds";
+import pluralise from "../../../lib/pluralise";
 
 export const likedSongsCover = "https://i.ibb.co/7bxRhFz/rnqa7yhv4il71.webp";
 
-const FavouritePage = (props) => {
+const FavouritePage = () => {
 	const { data } = useQuery(favouriteSongsKey, fetchFavouriteSongs);
+	const user = useContext(LoggedInUserContext);
+	if (!data) return <div key="no-data">No data available</div>;
 
-	const info = {
-		title: "Liked Songs",
-		thumbnail: likedSongsCover,
-		description: [""],
-		subtitle: "Playlist",
-	};
+	let totalLength: number = 0;
+	data?.forEach((el: Song) => (totalLength = totalLength + el.duration));
+
 	const gradientProps = {
-		image: info.thumbnail,
+		image: likedSongsCover,
 		roundAvatar: false,
-		description: info.description,
-		subtitle: info.subtitle,
-		title: info.title,
+		description: [
+			<Avatar
+				key="user-avatar"
+				size="xs"
+				sx={{ border: "2px solid black", marginRight: "5px" }}
+				src={user.avatarUrl ?? undefined}
+			/>,
+			<Text
+				key="user-name"
+				fontWeight="bold"
+			>{`${user.firstName} ${user.lastName}`}</Text>,
+			<Text key="separator" margin="0px 5px">
+				{"\u2022"}
+			</Text>,
+			<Text key="n-of-songs">
+				{data.length} {pluralise(data.length, "song")},
+			</Text>,
+			<Text key="total-length" marginLeft="5px">
+				{convertSeconds(totalLength, "hhhh mmmm ssss")}
+			</Text>,
+		],
+		subtitle: "Playlist",
+		title: "Liked Songs",
 	};
-
-	// console.log({ data });
-
-	// let topListItems: Track[] = query.cat === 'episodes' ? [...data].map(el => { id: el.id; thumbnail: el.avatarUrl; author: el.})
 
 	return (
 		<Box
@@ -52,139 +72,5 @@ const FavouritePage = (props) => {
 		</Box>
 	);
 };
-
-// export const getServerSideProps = async (
-// 	context: GetServerSidePropsContext
-// ) => {
-// 	// const response = await fetch("http://localhost:3000/api/favouritesongs");
-// 	// const jsonData = await response.json();
-
-// 	// console.log({ jsonData });
-// 	let info;
-// 	switch (context.query.cat) {
-// 		case "songs":
-// 			info = {
-// 				title: "Liked Songs",
-// 				thumbnail: "https://i.ibb.co/7bxRhFz/rnqa7yhv4il71.webp",
-// 				description: [""],
-// 				subtitle: "Playlist",
-// 			};
-// 			break;
-// 		case "episodes":
-// 			info = {
-// 				title: "Your Episodes",
-// 				thumbnail: "https://i.ibb.co/TPwSWwd/rnqa7yhv4il71.jpg",
-// 				description: [""],
-// 				subtitle: "Playlist",
-// 			};
-// 	}
-// 	return {
-// 		props: {
-// 			info,
-// 		},
-// 	};
-// };
-// export const getServerSideProps = async (context: GetServerSideProps) => {
-// const baseUrl = findBaseUrl();
-// const response = await fetch(baseUrl + "/api/favouritesongs");
-// if (!response.ok) throw new Error("Failed to fetch data");
-// console.log("json", response.json());
-// const user = await prisma.user.findUnique({
-// 	where: {
-// 		username: "saradc88",
-// 	},
-// });
-
-// const favouriteSongs = await fetch(baseUrl + "/api/me");
-// console.log({ favouriteSongs });
-
-// console.log(user);
-// };
-
-// interface CustomQuery extends ParsedUrlQuery {
-// 	username?: string;
-// 	params: any;
-// }
-
-// export const getServerSideProps = async (
-// 	context: GetServerSidePropsContext
-// ) => {
-// 	console.log({ context });
-// 	const customQuery: any = context.params;
-//     const user = useContext()
-// 	return {
-// 		props: {
-// 			result: { query: customQuery },
-// 		},
-// 	};
-// };
-// 	const user = await prisma.user.findUnique({
-// 		where: {
-// 			username: customQuery.username,
-// 		},
-// 		include: {
-// 			favouriteSongs: {
-// 				include: { album: true, artist: true, category: true },
-// 			},
-// 			createdPlaylists: { include: { category: true } },
-// 			favouritePlaylists: { include: { category: true } },
-// 			userFollowers: true,
-// 			artistFollowers: {
-// 				include: { albums: true, songs: true, category: true },
-// 			},
-// 			userFollowing: true,
-// 			artistFollowing: {
-// 				include: { albums: true, songs: true, category: true },
-// 			},
-// 		},
-// 	});
-// 	if (user && user.favouriteSongs && user.favouriteSongs) {
-// 		const userWithStats = {
-// 			...user,
-// 			stats: [
-// 				{
-// 					label: "playlist",
-// 					total: user.createdPlaylists.length + user.favouritePlaylists.length,
-// 				},
-// 				{
-// 					label: "follower",
-// 					total: user.userFollowers.length + user.artistFollowers.length,
-// 				},
-// 				{
-// 					label: "following",
-// 					total: user.userFollowing.length + user.artistFollowing.length,
-// 				},
-// 			],
-// 		};
-// 		return {
-// 			props: {
-// 				user: userWithStats,
-// 			},
-// 		};
-// 	}
-// };
-// const currentUser = await prisma.user.findUnique({
-// 	where: {
-// 		id: query.id,
-// 	},
-// 	include: {
-// 		category: true,
-// 		songs: {
-// 			include: {
-// 				album: true,
-// 				artist: true,
-// 				category: true,
-// 			},
-// 		},
-// 	},
-// });
-
-// if (playlist) {
-// 	return {
-// 		props: {
-// 			playlist,
-// 		},
-// 	};
-// }
 
 export default FavouritePage;
