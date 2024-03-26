941a397c44614cb2409171129354f3469d965cbf
Sara De Clara <saradeclara@gmail.com>
2024-03-26 15:06:16 +0000

    css fix for aligning grid elements in RecentlyAddedGrid

--- src/components/Dashboard/GridItem.tsx
+++ src/components/Dashboard/GridItem.tsx
@@ -1,38 +1,44 @@
 import { RecentlyAddedType } from "@/pages";
 import { Box, Img } from "@chakra-ui/react";
+import Link from "next/link";
 
 const GridItem = ({ item }: { item: RecentlyAddedType }) => {
 	return (
-		<Box
-			sx={{
-				background: "rgba(255,255,255,.3)",
-				flex: "1",
-				height: "70px",
-				borderRadius: "5px",
-				marginRight: "10px",
-				display: "inline-flex",
-			}}
-		>
-			<Box>
-				<Img
-					height="70px"
-					width="70px"
-					objectFit="cover"
-					borderRadius="5px 0 0 5px"
-					src={(item.avatarUrl || item.album?.avatarUrl) ?? undefined}
-				/>
-			</Box>
-			<Box
-				sx={{
-					height: "70px",
-					display: "flex",
-					alignItems: "center",
-					fontWeight: "bold",
-					marginLeft: "10px",
-				}}
-			>
-				{item.name}
-			</Box>
+		<Box sx={{ flex: "1" }}>
+			<Link href="">
+				<Box
+					sx={{
+						background: "rgba(255,255,255,.3)",
+						flex: "1",
+						height: "70px",
+						borderRadius: "5px",
+						marginRight: "10px",
+						display: "inline-flex",
+						width: "97%",
+					}}
+				>
+					<Box>
+						<Img
+							height="70px"
+							width="70px"
+							objectFit="cover"
+							borderRadius="5px 0 0 5px"
+							src={(item.avatarUrl || item.album?.avatarUrl) ?? undefined}
+						/>
+					</Box>
+					<Box
+						sx={{
+							height: "70px",
+							display: "flex",
+							alignItems: "center",
+							fontWeight: "bold",
+							marginLeft: "10px",
+						}}
+					>
+						{item.name}
+					</Box>
+				</Box>
+			</Link>
 		</Box>
 	);
 };
