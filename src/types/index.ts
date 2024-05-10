c1c36018beb6c9217a67833362deccc32e371e62
Sara De Clara <saradeclara@gmail.com>
2024-05-10 17:49:19 +0100

    app type file removed

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,8 @@
-import { AppPropsTypes } from "./app";
 import { AuthFormProps, FormDataType } from "./authForm";
 import { marginType } from "./playerLayout";
 import { menuDataType } from "./sidebar";
 import { jwtPayload, reqBodyTypes } from "./signin";
 export type {
-	AppPropsTypes,
 	AuthFormProps,
 	FormDataType,
 	jwtPayload,
